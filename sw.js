ac4f14371e5af148a5bf38575ee7953c80fa2e9d
wilfy2004 <jason.clarke@clarke-systems.com>
2024-10-05 17:04:06 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,28 +1,21 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2761
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
-\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open('v1').then((cache) => {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/styles.css',
+                '/app.js',
+                '/icon.png'
+            ]);
+        })
+    );
+});
 
-\f0\fs24 \cf0 self.addEventListener('install', (event) => \{\
-    event.waitUntil(\
-        caches.open('v1').then((cache) => \{\
-            return cache.addAll([\
-                '/',\
-                '/index.html',\
-                '/styles.css',\
-                '/app.js',\
-                '/icon.png'\
-            ]);\
-        \})\
-    );\
-\});\
-\
-self.addEventListener('fetch', (event) => \{\
-    event.respondWith(\
-        caches.match(event.request).then((response) => \{\
-            return response || fetch(event.request);\
-        \})\
-    );\
-\});}
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
